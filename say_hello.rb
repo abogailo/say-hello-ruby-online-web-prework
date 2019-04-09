1ed285cc90a999111bacbfe18524aa5956153374
Abagail Hudson <abagailanne@gmail.com>
2019-04-09 01:13:13 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,7 @@
 # Build your say_hello method here
 def say_hello(name)
-  phrase = "Hello #{name}!"
-  puts phrase
+  if name = null
+    puts = "Hello Ruby Programmer!"
+  else phrase = "Hello #{name}!"
+    puts phrase
 end
