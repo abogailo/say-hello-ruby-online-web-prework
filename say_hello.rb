e48f0b6572e41d3fe7bd1385d9c8d91caeb466cb
Abagail Hudson <abagailanne@gmail.com>
2019-04-09 01:22:29 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,7 @@
 # Build your say_hello method here
-def say_hello(options = {})
-   name = options[name] || "Ruby Programmer"
-    phrase = "Hello #{name}!"
+def say_hello(name)
+if name = nil
+   puts phrase = "Hello Ruby Programmer!"
+else phrase = "Hello #{name}!"
     puts phrase
 end
