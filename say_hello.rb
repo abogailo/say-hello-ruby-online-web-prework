424ecd853db171f40e52d31d7c52451860b99ddc
Abagail Hudson <abagailanne@gmail.com>
2019-04-09 01:20:49 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 def say_hello(options = {})
-   name = options[:name] || "Ruby Programmer"
+   name = options[name] || "Ruby Programmer"
     phrase = "Hello #{name}!"
     puts phrase
 end
